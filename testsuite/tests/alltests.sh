7762dc2c095ba22155251f42567ae33eb1a70365
root <root@debian-etch.(none)>
2010-03-10 06:33:40 +0100

    alltests.sh add, main test executer

---
+++ testsuite/tests/alltests.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export MKLLFS=../e2fsprogs-1.41.0.llfs/build/misc/mkllfs
+
+./test1.sh
+#./test2.sh
+#./test3.sh
+
+
--- testsuite/tests/cleanup.sh
+++ testsuite/tests/cleanup.sh
@@ -4,4 +4,4 @@ modprobe -r llfs
 echo "LOAD llfs module..."
 modprobe llfs
 echo "CLEANUP THE SYSTEM"
-../e2fsprogs-1.41.0.llfs/build/misc/mkllfs /dev/hdb1 > /dev/null
+$MKLLFS /dev/hdb1 > /dev/null
